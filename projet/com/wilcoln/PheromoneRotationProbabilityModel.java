7829c336542e661bda43289fb63ea27aa77c93fd
Wilfried Bounsi <wilcoln99@gmail.com>
2019-06-16 23:38:17 +0200

    Fix error in PheromoneRotationProbabilityModel

--- projet/com/wilcoln/PheromoneRotationProbabilityModel.java
+++ projet/com/wilcoln/PheromoneRotationProbabilityModel.java
@@ -6,18 +6,19 @@ import com.wilcoln.config.Config;
 public class PheromoneRotationProbabilityModel implements  AntRotationProbabilityModel {
     @Override
     public RotationProbability computeRotationProbs(RotationProbability movementMatrix, ToricPosition position, double directionAngle, AntEnvironmentView env) {
-
+        double[] angles = movementMatrix.getAngles();
+        double[] probs = movementMatrix.getProbabilities();
         double[] pheromoneQuantities = env.getPheromoneQuantitiesPerIntervalForAnt(position,directionAngle, movementMatrix.getAngles());
         double sum = 0;
-        for(int i = 0; i< movementMatrix.getProbabilities().length; i++){
-            movementMatrix.getProbabilities()[i] = movementMatrix.getProbabilities()[i]*Math.pow(detection(pheromoneQuantities[i]),Context.getConfig().getInt(Config.ALPHA));
-            sum += movementMatrix.getProbabilities()[i];
+        for(int i = 0; i < probs.length; i++){
+            probs[i] = probs[i]*Math.pow(detection(pheromoneQuantities[i]),Context.getConfig().getInt(Config.ALPHA));
+            sum += probs[i];
         }
-        for(int i = 0; i < movementMatrix.getProbabilities().length; i++){
-            movementMatrix.getProbabilities()[i] /= sum;
+        for(int i = 0; i < probs.length; i++){
+            probs[i] /= sum;
         }
 
-        return movementMatrix;
+        return new RotationProbability(angles, probs);
     }
     double detection(double x){
         return 1.0 / ( 1.0 + Math.exp( - Context.getConfig().getDouble(Config.BETA_D) * (x -  Context.getConfig().getDouble(Config.Q_ZERO)) ));
